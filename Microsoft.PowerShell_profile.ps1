db23eb8e88bc18a190b5325396ea08283ccda2dc
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-09-15 13:59:10 +0300

    feat(ps): add basic config

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Set-Alias vim nvim
+Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
+Set-PSReadLineKeyHandler -Key ctrl+n -Function NextHistory
+Set-PSReadLineKeyHandler -Key ctrl+p -Function PreviousHistory
