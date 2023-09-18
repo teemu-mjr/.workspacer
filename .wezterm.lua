bbd9e588528043e6f34cf35de0129671f05aaa82
Teemu Rintala <71847375+teemu-mjr@users.noreply.github.com>
2023-09-18 07:49:07 +0300

    feat(wezterm): create and close tabs

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,67 +16,45 @@ config.window_padding = {
     bottom = 0,
 }
 
+config.tab_and_split_indices_are_zero_based = true
+config.tab_bar_at_bottom = true
+
 config.leader = { key = "g", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-    {
-        key = "%",
-        mods = "LEADER|SHIFT",
-        action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-    },
-    {
-        key = "\"",
-        mods = "LEADER|SHIFT",
-        action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-    },
-    {
-        key = "x",
-        mods = "LEADER",
-        action = wezterm.action.CloseCurrentPane({ confirm = true }),
-    },
-    {
-        key = "[",
-        mods = "LEADER",
-        action = wezterm.action.ActivateCopyMode
-    },
+    { key = "%",  mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }), },
+    { key = "\"", mods = "LEADER|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }), },
+    { key = "c",  mods = "LEADER",       action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key = "z",  mods = "LEADER",       action = "TogglePaneZoomState" },
+    { key = "[",  mods = "LEADER",       action = wezterm.action.ActivateCopyMode },
     {
         key = "!",
         mods = "LEADER|SHIFT",
         action = wezterm.action_callback(function(win, pane)
-            local tab, window = pane:move_to_new_tab()
+            local tab, window =
+                pane:move_to_new_tab()
         end),
     },
-    {
-        key = "h",
-        mods = "LEADER|CTRL",
-        action = wezterm.action.ActivatePaneDirection "Left",
-    },
-    {
-        key = "j",
-        mods = "LEADER|CTRL",
-        action = wezterm.action.ActivatePaneDirection "Down",
-    },
-    {
-        key = "k",
-        mods = "LEADER|CTRL",
-        action = wezterm.action.ActivatePaneDirection "Up",
-    },
-    {
-        key = "l",
-        mods = "LEADER|CTRL",
-        action = wezterm.action.ActivatePaneDirection "Right",
-    },
+    { key = "h", mods = "LEADER|CTRL",  action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "j", mods = "LEADER|CTRL",  action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER|CTRL",  action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "l", mods = "LEADER|CTRL",  action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "H", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Left", 5 }) },
+    { key = "J", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Down", 5 }) },
+    { key = "K", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Up", 5 }) },
+    { key = "L", mods = "LEADER|SHIFT", action = wezterm.action.AdjustPaneSize({ "Right", 5 }) },
+    { key = "x", mods = "LEADER",       action = wezterm.action.CloseCurrentPane({ confirm = true }), },
 }
 
-for i = 1, 9 do
+for i = 0, 9 do
     table.insert(config.keys, {
         key = tostring(i),
         mods = "LEADER",
-        action = wezterm.action.ActivateTab(i - 1),
+        action = wezterm.action.ActivateTab(i),
     })
     table.insert(config.keys, {
         key = tostring(i),
         mods = "LEADER|CTRL",
-        action = wezterm.action.MoveTab(i - 1),
+        action = wezterm.action.MoveTab(i),
     })
 end
 
